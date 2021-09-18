4ea55b34c9078f6031dcf5ca997cc02470b4e9e1
GHOSSAN-AMMAR-S\a <ghossansnts@gmail.com>
2021-09-18 13:18:53 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// let year = 1999;
+// let month = 6;
+// let day = 15;
+// let name = "Ghossan Ammar Santos"
+
+// // console.log("Nama saya adalah " + name + " , " + " tanggal lahir saya adalah " + day + month + year);
+
+// console.log(year == month);
+// console.log(month === day);
+
+
+// function eyes(){
+//     console.log("Untuk Melihat");
+// }
+
+// function foot(){
+//     console.log("Untuk Berjalan");
+// }
+
+// eyes();
+
+function cekGanjilGenap(angka){
+    let bilangan = angka;
+    if(bilangan % 2 == 0)
+        console.log("Bilangan Genap")
+    else
+        console.log("Bilangan Ganjil");
+}
+
+cekGanjilGenap(2);
